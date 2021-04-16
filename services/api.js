b8cf03e185519d795079f205b731adbe180b12e8
Sandro Garcia <sndr95@hotmail.com>
2021-04-15 21:35:43 -0300

    correção no link da api

--- services/api.js
+++ services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const api = axios.create({
     //http://localhost:3333
-    // https://api-omie-etiquetas.herokuapp.com
-    baseURL: "http://localhost:3333"
+    // https://teste-telecom.herokuapp.com/
+    baseURL: "https://teste-telecom.herokuapp.com"
 })
 
 export default api
